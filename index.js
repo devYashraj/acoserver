7da56b479da9b7714dc721a3ed922a338b5f2c5d
devYashraj <capyashraj007@gmail.com>
2024-09-12 18:25:06 +0530

    fix backend

--- index.js
+++ index.js
@@ -22,12 +22,11 @@ const searchUrl = process.env.API_URL_SEARCH;
 const topUrl = process.env.API_URL_TOP;
 const api_key = process.env.API_KEY;
 
-axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
-
 app.post("/api/acoserver/search", async (req, res) => {
     const { query, country, language } = req.body;
     const url = `${searchUrl}?q=${query}&max=${30}&lang=${language}&country=${country}&apikey=${api_key}`;
     try {
+        console.log("Received search request");
         const response = await axios.get(url, { timeout: 10000 });
         res.status(200).json(response.data);
     } catch (error) {
@@ -40,6 +39,7 @@ app.post("/api/acoserver/topheadlines", async (req, res) => {
     const { country, language, category } = req.body;
     const url = `${topUrl}?category=${category}&max=${30}&lang=${language}&country=${country}&apikey=${api_key}`;
     try {
+        console.log("Received top headline request");
         const response = await axios.get(url, { timeout: 10000 });
         res.status(200).json(response.data);
     } catch (error) {
