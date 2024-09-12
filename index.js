a2ec0f678f0e232966890ac0902b3a7015151502
devYashraj <capyashraj007@gmail.com>
2024-09-12 19:16:51 +0530

    add https agent

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors';
 import axios from 'axios';
-import axiosRetry from 'axios-retry';
+import https from 'https';
+import http from 'http';
 
 dotenv.config()
 const app = express();
 app.use(express.json());
+
+const httpAgent = new http.Agent({ keepAlive: true, keepAliveMsecs: 15000 });  
+const httpsAgent = new https.Agent({ keepAlive: true, keepAliveMsecs: 15000 }); 
 
 const corsOptions = {
     origin: ['http://localhost:5173', 'https://aconews-8ea9b.web.app'],  
@@ -27,7 +31,10 @@ app.post("/api/acoserver/search", async (req, res) => {
     const url = `${searchUrl}?q=${query}&max=${30}&lang=${language}&country=${country}&apikey=${api_key}`;
     try {
         console.log("Received search request");
-        const response = await axios.get(url, { timeout: 10000 });
+        const response = await axios.get(url, {
+            httpsAgent, 
+            timeout: 10000  
+          });
         res.status(200).json(response.data);
     } catch (error) {
         console.log(error);
@@ -40,7 +47,10 @@ app.post("/api/acoserver/topheadlines", async (req, res) => {
     const url = `${topUrl}?category=${category}&max=${30}&lang=${language}&country=${country}&apikey=${api_key}`;
     try {
         console.log("Received top headline request");
-        const response = await axios.get(url, { timeout: 10000 });
+        const response = await axios.get(url,{
+            httpsAgent,  // Use httpAgent if it's an HTTP API
+            timeout: 10000  // Optional: Set a request timeout of 10 seconds
+          });
         res.status(200).json(response.data);
     } catch (error) {
         console.log(error);
